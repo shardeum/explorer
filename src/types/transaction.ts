3e70e5e8931d6c4c6595d218841667716f8d1eda
Jintu Das <jintu@shardeum.org>
2025-02-26 16:53:10 +0530

    Add TransferFromSecureAccount type to transaction enums

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -75,6 +75,7 @@ export enum InternalTXType {
   ChangeNetworkParam = 10,
   ApplyNetworkParam = 11,
   Penalty = 12,
+  TransferFromSecureAccount = 13,
 }
 
 /**
@@ -127,6 +128,7 @@ export enum TransactionSearchType {
   ChangeNetworkParam = 22,  //    |
   ApplyNetworkParam = 23,   //    |
   Penalty = 24,             // ----    
+  TransferFromSecureAccount = 25,
 }
 
 export interface Log {
