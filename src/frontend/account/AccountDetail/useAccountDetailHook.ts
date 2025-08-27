a009d552efb3cad005bf60be2ab621604f069b7d
Sam Sweet <samwsweet@gmail.com>
2025-08-27 13:32:56 -0400

    fix all ethers versioning issues

--- src/frontend/account/AccountDetail/useAccountDetailHook.ts
+++ src/frontend/account/AccountDetail/useAccountDetailHook.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
 import { api, PATHS } from '../../api'
 import { Account, AccountType, ContractType, Token, Transaction, TransactionSearchType } from '../../../types'
-import { BigNumberish, utils } from 'ethers'
+import { BigNumberish, formatUnits } from 'ethers'
 
 interface detailProps {
   id: string
@@ -88,7 +88,7 @@ export const useAccountDetailHook = ({ id, txType }: detailProps): AccountDetail
               (item: { contractType: ContractType; contractInfo: { decimals: string } | null; balance: BigNumberish }) => {
                 if (item.contractType === ContractType.ERC_20) {
                   const decimalsValue = item.contractInfo?.decimals ? parseInt(item.contractInfo.decimals) : 18
-                  item.balance = utils.formatUnits(item.balance, decimalsValue)
+                  item.balance = formatUnits(item.balance, decimalsValue)
                 }
               }
             )
