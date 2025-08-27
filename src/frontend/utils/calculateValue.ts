8d4551f8276573f1ed17417dd25b7d6d2e3817eb
Sam Sweet <samwsweet@gmail.com>
2025-08-27 16:16:53 -0400

    more ethers issues

--- src/frontend/utils/calculateValue.ts
+++ src/frontend/utils/calculateValue.ts
@@ -1,5 +1,5 @@
 import web3 from 'web3'
-import { ethers } from 'ethers'
+import { formatUnits } from 'ethers'
 import { TokenTx, TokenType, TransactionType } from '../../types'
 import BN from 'bn.js'
 import { fromWeiNoTrailingComma } from './fromWeiNoTrailingComma'
@@ -33,8 +33,8 @@ export const calculateTokenValue = (
       return tokenTx.tokenValue === '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
         ? 'unlimited'
         : fullValue
-        ? ethers.formatUnits(tokenTx.tokenValue, decimalsValue)
-        : roundTokenValue(ethers.formatUnits(tokenTx.tokenValue, decimalsValue))
+        ? formatUnits(tokenTx.tokenValue, decimalsValue)
+        : roundTokenValue(formatUnits(tokenTx.tokenValue, decimalsValue))
 
       // : round(web3.utils.fromWei(tokenTx.tokenValue, "ether"));
     } else if (txType === TokenType.ERC_721) {
